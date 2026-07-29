c1b43f928733906a25d2799449db9d291eb0f077
Z User <z@container>
2026-07-29 09:17:02 +0000

    8a20ea62-73d9-4ff4-b507-13c47280137a

--- src/components/sections/services.tsx
+++ src/components/sections/services.tsx
@@ -83,6 +83,8 @@ export function Services() {
             </span>
           </motion.h2>
 
+        </div>
+
         <div className="mt-16 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {services.map((s, i) => (
             <motion.article
