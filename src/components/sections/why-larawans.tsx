e8421254a2b1bec9e6a0275732cfa9eb3f474815
Z User <z@container>
2026-07-30 05:18:53 +0000

    299139fd-0e14-4189-8c4a-daceccb41a8a

--- src/components/sections/why-larawans.tsx
+++ src/components/sections/why-larawans.tsx
@@ -53,7 +53,7 @@ export function WhyLarawans() {
         </div>
 
         {/* Team member profile cards */}
-        <div className="mt-12 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="mt-[82px] grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
           {TEAM_MEMBERS.map((member, i) => (
             <motion.div
               key={member.name}
